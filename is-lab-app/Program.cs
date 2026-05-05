822d580eb58e79eab2114f390300b34ed6376afe
ekaterinalapsina403-netizen <ekaterinalapsina403@gmail.com>
2026-05-05 16:37:08 +0000

    Lab10

---
+++ is-lab-app/Program.cs
@@ -0,0 +1,74 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddOpenApi();
+
+var app = builder.Build();
+var notes = new List<Note>();
+app.MapGet("/health", () => new { status = "ok", time = DateTime.Now });
+
+app.MapGet("/version", (IConfiguration conf) => new {
+    name = conf["App:Name"],
+    version = conf["App:Version"]
+});
+app.MapGet("/api/notes", () => notes);
+
+app.MapPost("/api/notes", (Note note) => {
+    if (string.IsNullOrEmpty(note.Title)) return Results.BadRequest("Title is required");
+    notes.Add(note);
+    return Results.Created($"/api/notes/{note.Id}", note);
+});
+
+app.MapGet("/api/notes/{id}", (int id) =>
+    notes.FirstOrDefault(n => n.Id == id) is Note n ? Results.Ok(n) : Results.NotFound());
+
+app.MapDelete("/api/notes/{id}", (int id) => {
+    notes.RemoveAll(n => n.Id == id);
+    return Results.NoContent();
+});
+
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/db/ping", (IConfiguration conf) => {
+    var connectionString = conf.GetConnectionString("Mssql");
+    return Results.Problem($"Ïîïûòêà ïîäêëþ÷åíèÿ ê ÁÄ: {connectionString}. Îøèáêà: Áàçà äàííûõ åùå íå ðàçâåðíóòà.");
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
+public class Note
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
